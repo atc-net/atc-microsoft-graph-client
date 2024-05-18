69a93fae20159e7acba78e84c9e049b46138e6b4
Per Kops <per_kops@hotmail.com>
2024-05-18 11:02:30 +0200

    refact: change GraphServiceOptions to have all parameters as required - since its being used for construction of ClientSecretCredential

--- src/Atc.Microsoft.Graph.Client/Options/GraphServiceOptions.cs
+++ src/Atc.Microsoft.Graph.Client/Options/GraphServiceOptions.cs
@@ -4,7 +4,14 @@ public sealed class GraphServiceOptions
 {
     public string TenantId { get; set; } = string.Empty;
 
-    public string? ClientId { get; set; }
+    public string ClientId { get; set; } = string.Empty;
 
-    public string? ClientSecret { get; set; }
+    public string ClientSecret { get; set; } = string.Empty;
+
+    public bool IsValid() => !string.IsNullOrEmpty(TenantId) &&
+                             !string.IsNullOrEmpty(ClientId) &&
+                             !string.IsNullOrEmpty(ClientSecret);
+
+    public override string ToString()
+        => $"{nameof(TenantId)}: {TenantId}, {nameof(ClientId)}: {ClientId}, {nameof(ClientSecret)}: {ClientSecret}";
 }
