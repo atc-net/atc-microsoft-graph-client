60aad24308e02dc72135dbd624b1f0a3e3adcff0
Per Kops <per_kops@hotmail.com>
2024-05-18 15:33:51 +0200

    feat: introduce sample application, showing the wire-up and usage of ISharepointGraphService

---
+++ sample/Atc.Microsoft.Graph.Client.Sample/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.Net;
+global using Atc.Microsoft.Graph.Client.Extensions;
+global using Atc.Microsoft.Graph.Client.Options;
+global using Atc.Microsoft.Graph.Client.Services.Sharepoint;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
